50cf43d44aa9df71046dece1fbc6dfefa621a674
PedroAugcardozo <pedroaug.cardozo@gmail.com>
2025-10-21 11:26:22 -0300

    FEAT: configuracao de seguranca do projeto

--- saodamiao/src/main/java/com/example/saodamiao/Control/LoginControl.java
+++ saodamiao/src/main/java/com/example/saodamiao/Control/LoginControl.java
@@ -1,5 +1,11 @@
 package com.example.saodamiao.Control;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(name = "/login")
 public class LoginControl {
 
 }
